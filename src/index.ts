5de9aa6d95f1ff9f712a5576780bdeaf5418cec0
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2024-06-13 19:49:31 +0200

    Cors and cors options added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import router from './routes/scrape.route';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+const corsOptions = {
+    origin: '*', 
+    methods: ['GET'], 
+    allowedHeaders: ['Content-Type'], 
+};
+
+app.use(cors(corsOptions));
 
 app.use('/api/v1', router);
 
