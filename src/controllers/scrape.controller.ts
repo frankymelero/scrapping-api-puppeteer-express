5403bd6e2f8dce75fd061948ed8dce21fbe86207
Franky Melero <77981015+frankymelero@users.noreply.github.com>
2024-06-13 16:29:48 +0200

    Type modules assigned, improved error handling in controller

--- src/controllers/scrape.controller.ts
+++ src/controllers/scrape.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express';
 import { scrapeService } from '../services/scrape.service';
 
+// Type guard to check if the error is an instance of Error.
+const isError = (error: unknown): error is Error => {
+    return error instanceof Error;
+};
+
 export const scrapeController = {
     scrape: async (req: Request, res: Response) => {
         try {
@@ -10,8 +15,12 @@ export const scrapeController = {
             }
             const result = await scrapeService.scrapeWebsite(url);
             res.json({ result });
-        } catch (error: unknown | any) {
-            res.status(500).json({ error: error.message });
+        } catch (error: unknown) {
+            if (isError(error)) {
+                res.status(500).json({ error: error.message });
+            } else {
+                res.status(500).json({ error: 'Unknown error occurred' });
+            }
         }
     }
 };
